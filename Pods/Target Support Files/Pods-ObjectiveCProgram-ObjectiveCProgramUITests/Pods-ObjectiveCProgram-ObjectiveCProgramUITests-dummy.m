b15d21591a382f9b500ec7169e783981c4e6cbdc
MrZhou1010 <272587392@qq.com>
2020-06-20 14:00:51 +0800

    update

---
+++ Pods/Target Support Files/Pods-ObjectiveCProgram-ObjectiveCProgramUITests/Pods-ObjectiveCProgram-ObjectiveCProgramUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ObjectiveCProgram_ObjectiveCProgramUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ObjectiveCProgram_ObjectiveCProgramUITests
+@end
---
+++ Pods/Target Support Files/Pods-ObjectiveCProgramTests/Pods-ObjectiveCProgramTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ObjectiveCProgramTests : NSObject
+@end
+@implementation PodsDummy_Pods_ObjectiveCProgramTests
+@end
